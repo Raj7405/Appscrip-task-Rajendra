cbd4d6176ea3644375b6421b760c6204590ba92f
Raj7405 <rajendra.m.chaudhari2001@gmail.com>
2024-08-03 17:31:35 +0530

    card component created

---
+++ components/ui/card.js
@@ -0,0 +1,30 @@
+import Image from "next/image"
+import style from "@/styles/Card.module.css"
+
+const Card = ({productInfo}) => {
+    return(
+        <div className={style.card}>
+            <div className={style.cardImgContainer}>
+                <Image 
+                    src={productInfo.image}
+                    alt={productInfo.title}
+                    width={300}
+                    height={390}
+                    layout="intrinsic"
+                    className={style.cardImg}
+                />
+            </div>
+            <h3 className={style.cardTitle}>{productInfo.title}</h3>
+            <div className={style.cardPrice}>
+                <h4>${productInfo.price}</h4>
+                <img src="/icons/heart.svg" alt="heart"/>
+            </div>
+            <p className={style.cardInfo}>Sign in or Create an account to see pricing</p>
+            {
+                !productInfo.rating.count && <p className={style.stockOut}>OUT OF STOCK</p>     
+            }
+        </div>
+    )
+}
+
+export default Card;
